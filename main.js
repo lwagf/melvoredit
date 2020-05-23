d919074e35258bb5fa565e79e31822e4cc9ee635
Logan White <git@loganwhite.org>
2020-05-23 01:20:07 +0100

    Build the initial version

---
+++ main.js
@@ -0,0 +1,33 @@
+$(function() {
+    const $import = $('#txtImport');
+    const $edit = $('#txtEdit');
+    const $export = $('#txtExport');
+
+    $import.on('input', (e) => {
+        try {
+            const jsonSave = JSON.parse(pako.ungzip(atob($import.val()), { to: 'string' }));
+            $edit.val(JSON.stringify(jsonSave, null, 2));
+            $edit.attr('disabled', false);
+        } catch (e) {
+            $edit.val('');
+            $edit.attr('disabled', true);
+            $export.val('');
+            $export.attr('disabled', true);
+            return;
+        }
+    });
+
+    $edit.on('input', (e) => {
+        try {
+            const jsonSave = $edit.val();
+            JSON.parse(jsonSave);
+            const strSave = btoa(pako.gzip(jsonSave, { to: 'string' }));
+            $export.val(strSave);
+            $export.attr('disabled', false);
+        } catch (e) {
+            $export.val('');
+            $export.attr('disabled', true);
+            return;
+        }
+    });
+});
